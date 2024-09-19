575c4615e0ab7fca157087892477160a0cd54cdf
LiangYuxuan <lyx@lyx.es>
2024-09-19 10:59:14 +0800

    feat: zhCN locale

---
+++ src/locales/zhCN.ts
@@ -0,0 +1,52 @@
+export const specNames = new Map<number, string>([
+    [62, '奥法'], // Arcane Mage
+    [63, '火法'], // Fire Mage
+    [64, '冰法'], // Frost Mage
+    [65, '奶骑'], // Holy Paladin
+    [66, '防骑'], // Protection Paladin
+    [70, '惩戒'], // Retribution Paladin
+    [71, '武器'], // Arms Warrior
+    [72, '狂暴'], // Fury Warrior
+    [73, '防战'], // Protection Warrior
+    [102, '鸟德'], // Balance Druid
+    [103, '猫德'], // Feral Druid
+    [104, '熊德'], // Guardian Druid
+    [105, '奶德'], // Restoration Druid
+    [250, '鲜血'], // Blood Death Knight
+    [251, '冰霜'], // Frost Death Knight
+    [252, '邪恶'], // Unholy Death Knight
+    [253, '兽王'], // Beast Mastery Hunter
+    [254, '射击'], // Marksmanship Hunter
+    [255, '生存'], // Survival Hunter
+    [256, '戒律'], // Discipline Priest
+    [257, '神牧'], // Holy Priest
+    [258, '暗牧'], // Shadow Priest
+    [259, '刺杀'], // Assassination Rogue
+    [260, '狂徒'], // Outlaw Rogue
+    [261, '敏锐'], // Subtlety Rogue
+    [262, '元素'], // Elemental Shaman
+    [263, '增强'], // Enhancement Shaman
+    [264, '奶萨'], // Restoration Shaman
+    [265, '痛苦'], // Affliction Warlock
+    [266, '恶魔'], // Demonology Warlock
+    [267, '毁灭'], // Destruction Warlock
+    [268, '酒仙'], // Brewmaster Monk
+    [269, '踏风'], // Windwalker Monk
+    [270, '奶僧'], // Mistweaver Monk
+    [577, '浩劫'], // Havoc Demon Hunter
+    [581, '复仇'], // Vengeance Demon Hunter
+    [1467, '湮灭'], // Devastation Evoker
+    [1468, '奶龙'], // Preservation Evoker
+    [1473, '增辉'], // Augmentation Evoker
+]);
+
+export const mapNames = new Map<number, string>([
+    [507, '格瑞姆巴托'], // Grim Batol
+    [353, '围攻'], // Siege of Boralus
+    [375, '仙林'], // Mists of Tirna Scithe
+    [376, '通灵'], // The Necrotic Wake
+    [501, '宝库'], // The Stonevault
+    [502, '千丝'], // City of Threads
+    [503, '回响'], // Ara-Kara, City of Echoes
+    [505, '破晨号'], // The Dawnbreaker
+]);
