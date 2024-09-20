aef4c820f741f7ff515ef9bce4741583d3ed73e3
LiangYuxuan <lyx@lyx.es>
2024-09-20 17:33:59 +0800

    feat: add tier in analyse result

--- src/analyse.ts
+++ src/analyse.ts
@@ -1,6 +1,9 @@
+import ckmeans from './ckmeans.ts';
 import tInterval from './tInterval.ts';
 
 import type { Run } from './types.ts';
+
+const tierNames = ['S', 'A', 'B', 'C', 'D', 'F'];
 
 interface AnalyseResult {
     key: number,
@@ -8,6 +11,7 @@ interface AnalyseResult {
     mean: number,
     sd: number,
     ci: number,
+    tier: string,
     maxRun: {
         id: number,
         level: number,
@@ -36,7 +40,7 @@ export default (data: Map<number, Run[]>, limit = 500): AnalyseResult[] => {
     const allScore = [...data.values()].flat().map(({ score }) => score);
     const allScoreSD = allScore.length >= 2 ? calculateStandardDeviation(allScore) : 1;
 
-    const result = [...data.entries()].map(([key, runs]) => {
+    const analyzedData = [...data.entries()].map(([key, runs]) => {
         if (runs.length < 1) {
             const res: AnalyseResult = {
                 key,
@@ -44,6 +48,7 @@ export default (data: Map<number, Run[]>, limit = 500): AnalyseResult[] => {
                 mean: 0,
                 sd: 0,
                 ci: 0,
+                tier: 'F',
                 maxRun: {
                     id: 0,
                     level: 0,
@@ -65,6 +70,7 @@ export default (data: Map<number, Run[]>, limit = 500): AnalyseResult[] => {
                 mean: runs[0].score,
                 sd: 0,
                 ci: 0,
+                tier: 'F',
                 maxRun: {
                     id: runs[0].id,
                     level: runs[0].level,
@@ -82,7 +88,7 @@ export default (data: Map<number, Run[]>, limit = 500): AnalyseResult[] => {
         const sortedRuns = [...runs].sort((a, b) => b.score - a.score);
 
         const maxRun = sortedRuns[0];
-        const minRun = sortedRuns[sortedRuns.length - 1];
+        const minRun = sortedRuns[Math.min(sortedRuns.length, limit) - 1];
         const limited = sortedRuns.slice(0, limit).map(({ score }) => score);
 
         const sd = calculateStandardDeviation(limited);
@@ -96,6 +102,7 @@ export default (data: Map<number, Run[]>, limit = 500): AnalyseResult[] => {
             mean,
             sd,
             ci,
+            tier: 'F',
             maxRun: {
                 id: maxRun.id,
                 level: maxRun.level,
@@ -108,7 +115,18 @@ export default (data: Map<number, Run[]>, limit = 500): AnalyseResult[] => {
             },
         };
         return res;
-    });
+    }).sort((a, b) => a.ci - b.ci);
+
+    const buckets = ckmeans(analyzedData.map((r) => r.ci), 6).reverse();
+    const result = analyzedData.map((entry, index) => {
+        const bucketIndex = buckets.findIndex((bucket) => bucket <= index);
+        const tier = tierNames[bucketIndex];
+
+        return {
+            ...entry,
+            tier,
+        };
+    }).reverse();
 
     return result;
 };
