18ad8f577c51abf96cb19b85dbc54047b07c32a9
LiangYuxuan <lyx@lyx.es>
2024-09-24 14:40:20 +0800

    feat: standalone spec role

--- src/scripts/report.ts
+++ src/scripts/report.ts
@@ -6,6 +6,9 @@ import analyse from '../analyse.ts';
 import fetcher from '../fetcher.ts';
 import { RIO_MIN_LEVEL } from '../config.ts';
 import {
+    tanks, healers, melees, rangeds,
+} from '../data.ts';
+import {
     textDungeon,
     textTank,
     textHealer,
@@ -17,70 +20,12 @@ import {
 
 import type { Run } from '../types.ts';
 
-const tanks = {
-    name: textTank,
-    specs: [
-        250, // Blood Death Knight
-        581, // Vengeance Demon Hunter
-        104, // Guardian Druid
-        268, // Brewmaster Monk
-        66, // Protection Paladin
-        73, // Protection Warrior
-    ],
-};
-
-const healers = {
-    name: textHealer,
-    specs: [
-        105, // Restoration Druid
-        1468, // Preservation Evoker
-        270, // Mistweaver Monk
-        65, // Holy Paladin
-        256, // Discipline Priest
-        257, // Holy Priest
-        264, // Restoration Shaman
-    ],
-};
-
-const melees = {
-    name: textMelee,
-    specs: [
-        251, // Frost Death Knight
-        252, // Unholy Death Knight
-        577, // Havoc Demon Hunter
-        103, // Feral Druid
-        255, // Survival Hunter
-        269, // Windwalker Monk
-        70, // Retribution Paladin
-        259, // Assassination Rogue
-        260, // Outlaw Rogue
-        261, // Subtlety Rogue
-        263, // Enhancement Shaman
-        71, // Arms Warrior
-        72, // Fury Warrior
-    ],
-};
-
-const rangeds = {
-    name: textRanged,
-    specs: [
-        102, // Balance Druid
-        1467, // Devastation Evoker
-        1473, // Augmentation Evoker
-        253, // Beast Mastery Hunter
-        254, // Marksmanship Hunter
-        62, // Arcane Mage
-        63, // Fire Mage
-        64, // Frost Mage
-        258, // Shadow Priest
-        262, // Elemental Shaman
-        265, // Affliction Warlock
-        266, // Demonology Warlock
-        267, // Destruction Warlock
-    ],
-};
-
-const specsData = [tanks, healers, melees, rangeds];
+const specsData = [
+    { name: textTank, specs: tanks },
+    { name: textHealer, specs: healers },
+    { name: textMelee, specs: melees },
+    { name: textRanged, specs: rangeds },
+];
 
 const dungeonXRuns = new Map<number, Run[]>();
 const specXRuns = new Map<number, Run[]>();
