610f8bfaed7d6ec0a427a588f6cbf60fd7b7562b
LiangYuxuan <lyx@lyx.es>
2024-12-01 02:04:25 +0800

    feat: min level change back to 7 as it auto inc in later week in season

--- src/updater/config.ts
+++ src/updater/config.ts
@@ -4,7 +4,4 @@ export const RIO_EXPANSION_ID = 10;
 
 export const RIO_SEASON = 'season-tww-1';
 
-// used in early weeks of the season
-// export const RIO_MIN_LEVEL = 7;
-
-export const RIO_MIN_LEVEL = 12;
+export const RIO_MIN_LEVEL = 7;
