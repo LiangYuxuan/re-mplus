5c0d1c017a155613b185b9e8cfd1a3898b90ebbd
LiangYuxuan <lyx@lyx.es>
2025-03-01 17:28:08 +0800

    feat: current season set to The War Within Season 1 Post Season

--- src/updater/config.ts
+++ src/updater/config.ts
@@ -2,6 +2,6 @@ import seasons from '../data/generated/seasons.json' with { type: 'json' };
 
 export const RIO_MAX_PAGE = 250;
 
-export const RIO_CURRENT_SEASON: keyof typeof seasons = 'season-tww-1';
+export const RIO_CURRENT_SEASON: keyof typeof seasons = 'season-tww-1-post';
 
 export const RIO_CURRENT_SEASON_MIN_LEVEL = 7;
